ab2f95ed919102296d90718d73ac65cb68da9f94
Gian Mateus <gian.m.silver@gmail.com>
2025-12-30 00:32:58 -0300

    Add Card component and styles

---
+++ src/components/card/card.tsx
@@ -0,0 +1,10 @@
+interface CardProps {
+  children: React.ReactNode;
+}
+export function Card({ children }: CardProps) {
+    return (
+        <div className="card">
+            {children}
+        </div>
+    );
+}
